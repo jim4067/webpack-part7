7395dbaf676fb8edb8d17471b58e5a43c3121341
jim4067 <jimmyimpulse2@gmail.com>
2020-10-21 12:44:22 +0300

    feat: added babel polyfill for working with async js code

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 
 const config = {
-    entry :'./src/index.js',
-    output : {
-        path : path.resolve(__dirname, 'build'),
+    entry: ['@babel/polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'build'),
         filename: 'main.js'
     },
-    module : {
-        rules : [
+    module: {
+        rules: [
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 options: {
-                    presets : ['@babel/preset-react'],
+                    presets: ['@babel/preset-react'],
                 },
             },
         ],
